cbe0e8a7f0eb8945ac47bc6c11d6d9c3d8e92274
Misael Ntirushwa <misaelntirushwa@gmail.com>
2021-02-19 12:59:16 -0500

    added test containers

---
+++ src/test/java/dev/misael/todowebservice/repository/ToDoRepositoryIntegrationTest.java
@@ -0,0 +1,65 @@
+package dev.misael.todowebservice.repository;
+
+import dev.misael.todowebservice.model.ToDoItem;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
+@Testcontainers
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
+@ContextConfiguration(initializers = { ToDoRepositoryIntegrationTest.Initializer.class })
+public class ToDoRepositoryIntegrationTest {
+
+    @Autowired
+    private ToDoRepository toDoRepository;
+
+    @Container
+    public static PostgreSQLContainer postgreSQLContainer = createDbContainer();
+
+    private static PostgreSQLContainer createDbContainer() {
+        return new PostgreSQLContainer("postgres:9.6.10-alpine")
+                .withUsername("postgres")
+                .withPassword("postgres")
+                .withDatabaseName("todo");
+    }
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                    "spring.datasource.password=" + postgreSQLContainer.getPassword(),
+                    "spring.datasource.driver-class-name=org.postgresql.Driver",
+                    "spring.jpa.generate-ddl=true"
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+
+    @Test
+    public void testCanSaveNewToDoItem() {
+        ToDoItem toDoItem = createToDoItem("Buy milk");
+        assertNull(toDoItem.getId());
+        toDoRepository.save(toDoItem);
+        assertNotNull(toDoItem.getId());
+    }
+
+    private ToDoItem createToDoItem(String name) {
+        ToDoItem toDoItem = new ToDoItem();
+        toDoItem.setName(name);
+        return toDoItem;
+    }
+}
